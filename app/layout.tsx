edd8fe4edcd4fb0608f73905308856b0dc3b90d1
Khairov Marat <khairovm9975@gmail.com>
2024-04-07 23:43:14 +0300

    next auth credentials signin

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import Navbar from "@/components/templates/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <main className="h-screen flex flex-col justify-center items-center">
+          <Navbar />
+          {children}
+        </main>
         <Toaster />
       </body>
     </html>
---
+++ components/templates/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { HandMetal } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+
+const Navbar = () => {
+  return (
+    <div className=" bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <HandMetal />
+        </Link>
+        <Link className={buttonVariants()} href="/auth/login">
+          Sign in
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
