b589e1246eb39a1d3de1c5a9939ce902dac5a52b
Khairov Marat <khairovm9975@gmail.com>
2024-04-11 01:41:51 +0300

    fix auth callbacks

--- components/templates/Navbar.tsx
+++ components/templates/Navbar.tsx
@@ -1,17 +1,23 @@
 import Link from "next/link";
-import { HandMetal } from "lucide-react";
 import { buttonVariants } from "@/components/ui/button";
+import { getServerSession } from "next-auth";
+import { SignOutButton } from "../ui/signOutButton";
+import { authOptions } from "@/lib/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
     <div className=" bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
       <div className="container flex items-center justify-between">
-        <Link href="/">
-          <HandMetal />
-        </Link>
-        <Link className={buttonVariants()} href="/auth/login">
-          Sign in
-        </Link>
+        <Link href="/">FitCore</Link>
+        {session?.user ? (
+          <SignOutButton />
+        ) : (
+          <Link className={buttonVariants()} href="/auth/login">
+            Войти
+          </Link>
+        )}
       </div>
     </div>
   );
