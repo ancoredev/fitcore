b0abb1c26cbed7ddc1d69f147e31e18239899b46
Khairov Marat <khairovm9975@gmail.com>
2024-04-12 00:22:12 +0300

    username field in session and session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import Navbar from "@/components/templates/Navbar";
+import { Providers } from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,11 +20,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="h-screen flex flex-col justify-center items-center">
-          <Navbar />
-          {children}
-        </main>
-        <Toaster />
+        <Providers>
+          <main className="h-screen flex flex-col justify-center items-center">
+            <Navbar />
+            {children}
+          </main>
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
